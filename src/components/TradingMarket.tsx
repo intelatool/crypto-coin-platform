8d98ac669967ab42c370a2d902c544365bfc556f
intelatool <teslasradiant@gmail.com>
2025-08-19 09:38:07 -0700

    Move TradingMarket.tsx to src/components/TradingMarket.tsx

---
+++ src/components/TradingMarket.tsx
@@ -0,0 +1,14 @@
+// Content of the TradingMarket component
+
+import React from 'react';
+
+const TradingMarket = () => {
+    return (
+        <div>
+            <h1>Trading Market</h1>
+            {/* Trading market content goes here */}
+        </div>
+    );
+};
+
+export default TradingMarket;
